f459cc0f236a27792e08bbc383f75ae3db38a7e7
Carson Gee <x@carsongee.com>
2017-12-02 17:40:35 -0500

    Added github pull requests to dashboard

--- start.py
+++ start.py
@@ -1,16 +1,37 @@
 import io
-from os.path import dirname, join, realpath
+from os import environ as env
+from os.path import abspath, dirname, join, realpath
 from random import randint
 
 from flask import Flask, render_template, jsonify
+from flask_htpasswd import HtPasswdAuth
 import requests
 import yaml
 
+API_PREFIX = '/start-api/v1/'
 BING = 'https://www.bing.com'
 PATH = dirname(realpath(__file__))
 CONFIG = yaml.load(io.open(join(PATH, 'data.yml'), 'r', encoding='UTF-8'))
+GH_URL = 'https://api.github.com/'
+GH_HEADERS = {'Authorization': 'token {}'.format(env.get('GH_TOKEN'))}
+GH_USERNAME = env.get('GH_USERNAME')
+GH_SEARCH_PREFIX = 'search/issues?q=state:open type:pr '
+GH_SEARCHES = (
+    ('review-requests', 'review-requested:{}'.format(GH_USERNAME)),
+    ('changes-requested', 'review:changes_requested author:{}'.format(GH_USERNAME)),
+    ('approved', 'review:approved author:{}'.format(GH_USERNAME)),
+)
 
 app = Flask(__name__, static_folder='./dist/static', template_folder='./dist/')
+app.config['FLASK_AUTH_ALL'] = True
+app.config['FLASK_HTPASSWD_PATH'] = abspath('.htpasswd')
+htpasswd = HtPasswdAuth(app)
+
+# Write out htpasswd to path if environment variable set
+HTPASSWD = env.get('START_HTPASSWD')
+if HTPASSWD:
+    with open(FLASK_HTPASSWD_PATH, 'w') as wfile:
+        wfile.write(HTPASSWD)
 
 
 def rando(list_like):
@@ -20,13 +41,14 @@ def rando(list_like):
     return list_like[randint(0, len(list_like) - 1)]
 
 
-@app.route('/start-api/v1/config')
+@app.route(API_PREFIX + 'config')
 def config():
     """Return our configuration to client."""
     data = {}
     data['greeting'] = rando(CONFIG['greetings'])
     data['fixed_links'] = CONFIG['fixed_links']
     data['name'] = CONFIG['name']
+    # TODO: Move these off into javascript since they are authless
     try:
         response = requests.get(
             BING + '/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US'
@@ -48,6 +70,21 @@ def config():
 
     return jsonify(data)
 
+@app.route(API_PREFIX + 'github')
+def github():
+    """Get pull requests and any other github related data."""
+    data = {}
+    for key, search in GH_SEARCHES:
+        try:
+            response = requests.get(
+                GH_URL + GH_SEARCH_PREFIX + search,
+                headers=GH_HEADERS,
+                timeout=5,
+            ).json()
+            data[key] = response['items']
+        except requests.RequestException:
+            pass
+    return jsonify(data)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -57,3 +94,4 @@ def catch_all(path):
     if app.debug:
         return requests.get('http://localhost:8080/{}'.format(path)).text
     return render_template("index.html")
+
